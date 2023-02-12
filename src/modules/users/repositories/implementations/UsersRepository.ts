ad0d40c039a0314325e8349314b344f2f4f4ff88
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-02-12 11:02:26 -0300

    solve challenge

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,24 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository.findOne({id: user_id}, {
+      relations: ["games"]
+    }) as User;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository
+      .query("SELECT id, first_name, last_name, email from USERS ORDER BY first_name;");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository
+      .query(
+        "SELECT id, first_name, last_name, email FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = LOWER($2)",
+        [first_name, last_name]
+      );
   }
 }
